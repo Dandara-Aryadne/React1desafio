1481a63f2e26bc8ee3b42641b22c8eba287d6a52
Dandara Aryadne <94024628+Dandara-Aryadne@users.noreply.github.com>
2022-02-03 20:38:49 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Passando o desafio com os doguinhos</p>
+      </div>
+    );
+  }
+}
+
+export default App;
